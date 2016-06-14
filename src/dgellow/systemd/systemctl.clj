01d413a0ecf07c412b040e317318795c285ba08c
dgellow <samuel.elborai@gmail.com>
2016-06-14 22:50:06 +0700

    use some macros to avoid repetitions

--- src/dgellow/systemd/systemctl.clj
+++ src/dgellow/systemd/systemctl.clj
@@ -2,72 +2,46 @@
   (:require [clojure.java.shell :refer [sh]]))
 
 (defn systemctl [& args]
-  (apply sh "systemctl" "--no-pager"
+  (apply sh "systemctl" "--no-pager" "--no-legend"
     (map (comp str name) (filter identity args))))
 
 (defn- parse-list* [output]
-  (->> output
-     clojure.string/split-lines
-     (map clojure.string/trim)
-     (map #(clojure.string/replace % "  " " "))
-     (map #(clojure.string/split % #" "))
-     (map #(filter (fn [x] (not (clojure.string/blank? x))) %))))
+   (->> output
+      clojure.string/split-lines
+      (map clojure.string/trim)
+      (map #(clojure.string/replace % "  " " "))
+      (map #(clojure.string/split % #" "))
+      (map #(filter (fn [x] (not (clojure.string/blank? x))) %))))
 
-(defn parse-list-units [output]
-  (->> output
-     parse-list*
-     (drop 1)
-     (drop-last 7)
-     (map (fn [[unit load active sub & descr]]
-            {:description (clojure.string/join " " descr)
-             :unit unit
-             :load load
-             :active active
-             :sub sub}))))
+(defmacro deflistcommand* [name map-fn]
+  `(defn ~name
+     ([] (~name nil))
+     ([pattern# & opts#]
+      (let [ret# (apply systemctl (keyword '~name) pattern# opts#)
+            {exit# :exit
+             out# :out} ret#]
+        (if-not (= exit# 0)
+          ret#
+          (->> out#
+             parse-list*
+             (map ~map-fn)))))))
 
-(defn list-units
-  ([] (list-units nil))
-  ([pattern & opts]
-   (let [{:keys [exit out err] :as ret}
-         (apply systemctl :list-units :--all pattern opts)]
-     (if-not (= exit 0)
-       ret
-       (parse-list-units out)))))
+(defmacro ^{:private true} deflistcommand [name keywords]
+  `(deflistcommand* ~name
+     (fn [[~@(map symbol keywords)]]
+       ~(apply hash-map
+          (flatten
+           (map (fn [x] [(keyword x) x])
+             keywords))))))
 
-(defn parse-list-unit-files [output]
-  (->> output
-     parse-list*
-     (drop 1)
-     (drop-last 2)
-     (map (fn [[unit-file state]] {:unit-file unit-file :state state}))))
+(deflistcommand* list-units
+  (fn [[unit load active sub & descr]]
+    {:description (clojure.string/join " " descr)
+     :unit unit
+     :load load
+     :active active
+     :sub sub}))
 
-(defn list-unit-files
-  ([] (list-unit-files nil))
-  ([pattern & opts]
-   (let [{:keys [exit out err] :as ret}
-         (apply systemctl :list-unit-files pattern opts)]
-     (if-not (= exit 0)
-       ret
-       (parse-list-unit-files out)))))
-
-(defn parse-list-timers [output]
-  (->> output
-     parse-list*
-     (drop 1)
-     (drop-last 2)
-     (map (fn [[next left last passed unit activates]]
-            {:next next
-             :left left
-             :last last
-             :passed passed
-             :unit unit
-             :activates activates}))))
-
-(defn list-timers
-  ([] (list-timers nil))
-  ([pattern & opts]
-   (let [{:keys [exit out err] :as ret}
-         (apply systemctl :list-timers :--all pattern opts)]
-     (if-not (= exit 0)
-       ret
-       (parse-list-timers out)))))
+(deflistcommand list-sockets [listen unit activates])
+(deflistcommand list-unit-files [unit-file state])
+(deflistcommand list-timers [next left last passed unit activates])
